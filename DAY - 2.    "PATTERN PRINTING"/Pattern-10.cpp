598a9bdfc9e02564833ca2963c6c2b352dfd8111
Sonam Narula <sonamnarula2108@gmail.com>
2025-10-14 16:58:37 +0530

    Create Pattern-10.cpp

---
+++ DAY - 2.    "PATTERN PRINTING"/Pattern-10.cpp
@@ -0,0 +1,28 @@
+// Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
+// Print the pattern in the function given to you.
+
+class Solution {
+public: 
+    void pattern10(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i > n) stars = 2 * n - i;
+            for (int j = 1; j <= stars; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+
+};
