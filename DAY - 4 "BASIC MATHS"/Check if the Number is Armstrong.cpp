c38d9998b279147559d0e045577cd13a6dfb9644
Sonam Narula <sonamnarula2108@gmail.com>
2025-10-16 15:09:18 +0530

    Create Check if the Number is Armstrong.cpp

---
+++ DAY - 4 "BASIC MATHS"/Check if the Number is Armstrong.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int countDigit(int n) {
+        int count = log10(n) + 1;
+        return count;
+    }
+    
+public:
+
+    bool isArmstrong(int n) {
+        int count = countDigit(n);
+        int sum = 0;
+        int copy = n;
+        while(n > 0){
+            int lastDigit = n % 10;
+            sum += pow(lastDigit, count); 
+            n = n / 10;
+        }
+        if(sum == copy) return true;
+        return false;
+    }
+};
